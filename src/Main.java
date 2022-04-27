ec69dcd528c341e6fcd952e391565557c871afdd
ori mamo <mori8056@gmail.com>
2022-04-27 16:51:28 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+
+public class Main extends JFrame {
+    public static final int WINDOW_WIDTH = 800  ;
+    public static final int WINDOW_HEIGHT = 800 ;
+
+    public static void main(String[] args) {
+        new Main();
+    }
+
+    public Main () {
+        GalagaPanel galagaPanel = new GalagaPanel(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+        this.add(galagaPanel);
+        this.setResizable(false);
+        this.setLayout(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        this.setVisible(true);
+
+
+
+    }
+
+
+
+}
